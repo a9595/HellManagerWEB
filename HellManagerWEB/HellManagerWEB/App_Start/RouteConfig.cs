12b977c5535e20ce2fc5061949928562e07acbdb
Andrii Kovalchuk tieorange <tieorange@gmail.com>
2015-05-26 22:29:55 +0200

    set def page to sinners

--- HellManagerWEB/HellManagerWEB/App_Start/RouteConfig.cs
+++ HellManagerWEB/HellManagerWEB/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace HellManagerWEB
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Sinners", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
